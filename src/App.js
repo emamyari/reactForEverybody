34569f0bf5ead2bc0c11244e5718ff710f9ee081
emamyari <emamyari@yahoo.com>
2022-04-28 18:09:03 +0430

    add project item

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/counter';
+
+function App() {
+  
+  return (
+    <div>
+      <Counter></Counter>
+      {/* <Counter></Counter>
+      <Counter></Counter>
+      <Counter></Counter> */}
+    </div>
+    );
+}
+
+export default App;
