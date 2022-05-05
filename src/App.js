02a0a84a5d921920ff33e17f0f5ef8a7511692ae
emamyari <emamyari@yahoo.com>
2022-05-05 18:13:53 +0430

    add counters with dynamic method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Counter from './components/counter';
 
-function App() {
-  
-  return (
-    <div>
-      <Counter></Counter>
-    </div>
-    );
-}
 
-export default App;
+class App extends Component {
+  state = { counters:[
+                      {id:1,value:"آیفون"},
+                      {id:2},
+                      {id:3},
+                      {id:6}
+                      ] } 
+  render() { 
+
+    console.log(this.state.counters)
+    return ( <div>
+               {this.state.counters.map(c=><div>
+                                                <Counter   key={c.id} /> 
+                                                <div> {c.value}</div>
+                                            </div> )
+                } 
+              </div>);
+  }
+}
+ 
+export default App ;
+
+
+
