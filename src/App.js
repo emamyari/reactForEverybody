7ed3f3ddc8b7220de105bb5966fc3118a34ec1e0
emamyari <emamyari@yahoo.com>
2022-05-05 17:09:30 +0430

    add increment arrow function to counter component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
   return (
     <div>
       <Counter></Counter>
-      {/* <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter> */}
     </div>
     );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
  class Counter extends Component {
-    state = { count:10,
-    imgUrl:"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
-    tag:['tag1','tag2','tag3','tag4','tag5','tag6']
-    } 
-    // style={
-    //     fontWeight:600,
-    //     fontSize:'100px'
-    // }
+    state = { count:12 } 
+   
+     handleInc = () => {
+        this.setState( { count:this.state.count+1 } )
+     }
+
     render() { 
         return (<div>
-                {/* <img src={this.state.imgUrl}></img> */}
-                <button className='btn btn-danger m-2' >dec.</button>
+                 <button    className='btn btn-danger m-2' >dec.</button>
                 <span  className= {this.getClass()}>{this.formatCount()}</span>
-                <button className='btn btn-success m-2'>Inc.</button>
-                {this.state.tag.map(c=><div>{c}</div>)}
-               </div>);
+                <button onClick={this.handleInc} className='btn btn-success m-2'>Inc.</button>
+                </div>);
     }
 
     formatCount(){
+          
         return this.state.count===0? ' zero ' : this.state.count
     }
 
