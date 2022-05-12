c20eceb721ea4ae1d4e96e5f852afd92a3610404
emamyari <emamyari@yahoo.com>
2022-05-12 16:59:49 +0430

    ui organization

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import Counter from './components/counter';
 
 
 class App extends Component {
-  state = { counters:[
-                      {id:1,value:"آیفون"},
-                      {id:2},
-                      {id:3},
-                      {id:6}
-                      ] } 
-  render() { 
+  state = {
+    counters: [
+      { id: 1, value: "آیفون" },
+      { id: 2, value: "گاغذ" },
+      { id: 3, value: "جارو" },
+      { id: 6, value: "مک بوک" }
+    ]
+  }
+  render() {
 
     console.log(this.state.counters)
-    return ( <div>
-               {this.state.counters.map(c=><div>
-                                                <Counter   key={c.id} /> 
-                                                <div> {c.value}</div>
-                                            </div> )
-                } 
-              </div>);
+    return (<div className='row'>
+      {this.state.counters.map(c => <div  >
+        <Counter key={c.id} />
+        <div className='btn btn-primary m-2 '> {c.value}</div>
+      </div>)
+      }
+    </div>);
   }
 }
- 
-export default App ;
+
+export default App;
 
 
 
