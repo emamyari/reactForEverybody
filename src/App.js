930d4b49da0e91715792952d8807f06646c223bf
emamyari <emamyari@yahoo.com>
2022-07-12 19:21:54 +0430

    accept all changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,68 @@ import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 import Counters from './components/counters';
-import NavBar  from './components/navbar';
+import NavBar from './components/navbar';
 
 
 class App extends Component {
   state = {
+    counters: [
+      { id: 1, value: "آیفون", count: 8 },
+      { id: 2, value: "کاغذ", count: 100 },
+      { id: 3, value: "جارو", count: 150 },
+      { id: 6, value: "مک بوک", count: 10 }
+    ]
+  }
+
+  handleInc = counter => {
+    console.log(counter)
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+    counters[index] = { ...counter }
+    counters[index].count++;
+    this.setState({ counters })
+    // var co=this.state.count
+    // var c=  co>=10 ? co : co+1
+    // this.setState( { count:c } )
+  }
+
+  handleDec = (counter) => {
+    console.log(counter)
+    const counters = [...this.state.counters]
+    const index = counters.indexOf(counter)
+    counters[index] = { ...counter }
+    counters[index].count--;
+    this.setState({ counters })
+  }
+
+  handleDelete = (counterId) => {
+    // console.log('handle delete is runing',counterId)
+    const counters = this.state.counters.filter(c => c.id != counterId)
+    this.setState({ counters })
+  }
+
+  componentDidMount() {
+
+    fetch("http://192.168.1.116:7000/test/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({ counters: result })
+        },
+        (error) => {
+        }
+      )
 
   }
 
   render() {
 
-    return (<div>  
+    return (<div>
       <NavBar></NavBar>
-      <Counters         />
-      </div> );
+      <Counters
+        cntr={this.state.counters}
+        del={this.handleDelete} />
+    </div>);
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,54 +1,21 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: "آیفون", count: 8 },
-            { id: 2, value: "کاغذ", count: 100 },
-            { id: 3, value: "جارو", count: 150 },
-            { id: 6, value: "مک بوک", count: 10 }
-        ]
-    }
-
-    handleInc = counter => {
-        console.log(counter)
-        const counters=[...this.state.counters]
-        const index=counters.indexOf(counter)
-        counters[index]={...counter}
-        counters[index].count++;
-        this.setState({counters})
-         // var co=this.state.count
-        // var c=  co>=10 ? co : co+1
-        // this.setState( { count:c } )
-     }
-
-     handleDec = (counter) => {
-        console.log(counter)
-        const counters=[...this.state.counters]
-        const index=counters.indexOf(counter)
-        counters[index]={...counter}
-        counters[index].count--;
-        this.setState({counters})
-    }
    
-     handleDelete = (counterId) => {
-            // console.log('handle delete is runing',counterId)
-        const counters = this.state.counters.filter(c => c.id != counterId)
-        this.setState({ counters })
-    }
-
     render() {
+        console.log("render")
+
         return (<div className='row'>
 
             {
-                this.state.counters.map(item => <div  >
+                this.props.cntr.map(item => <div  >
                     <Counter 
                             key={item.id}
                             id={item.id} 
                             name={item.value} 
                             count={item.count} 
                             counter={item}
-                            onDelete={this.handleDelete}
+                            onDelete={this.props.del}
                             onInc={this.handleInc}
                             onDec={this.handleDec}
                             />
