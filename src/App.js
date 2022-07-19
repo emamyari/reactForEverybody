f3125f9a054a0fec692bfeeaf4f17c3d67733511
emamyari <emamyari@yahoo.com>
2022-07-19 19:48:24 +0430

    chart type changing

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@
   import Highcharts from 'highcharts';
   import HighchartsReact from 'highcharts-react-official';
 
-const option={
+let option={
+  chart: {
+    type: 'pie'
+},
   series:[{
-    name:'nomre',
-    data:[10,15,20,16,50]
-  }]
+    name: 'Share',
+    data: [
+        { name: 'Chrome', y: 61.41 },
+        { name: 'Internet Explorer', y: 11.84 },
+        { name: 'Firefox', y: 10.85 },
+        { name: 'Edge', y: 4.67 },
+        { name: 'Safari', y: 4.18 },
+        { name: 'Other', y: 7.05 }
+    ]
+}]
 }
 
 
